41c660fdda6972deef457f63f81ffa780424748a
andydockett <adockett@artemedios.com>
2017-07-24 15:16:05 -0400

    environments.ts
    added environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyArsLvm03z3FGLappY8oMWrz_B9EKwd5rA",
+    authDomain: "gavalpoprod.firebaseapp.com",
+    databaseURL: "https://gavalpoprod.firebaseio.com",
+    storageBucket: "gavalpoprod.appspot.com"
+  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// https://angularfirebase.com/lessons/development-and-production-environments-for-angular-firebase-projects/
+
 export const environment = {
-  production: false
+  production: false,
+  	firebaseConfig: {
+      apiKey: "AIzaSyAbdcqUi85_mnxGhvnZohm-Gd9LywFwxPc",
+    authDomain: "gavalpodev.firebaseapp.com",
+     databaseURL: "https://gavalpodev.firebaseio.com",
+     storageBucket: "gavalpodev.appspot.com"
+  }
 };
+
